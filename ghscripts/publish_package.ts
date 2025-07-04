1931b563ea607d203ec22f345f34775fefb5f687
justin <justin@163.com>
2025-07-04 10:13:26 +0800

    update

--- ghscripts/publish_package.ts
+++ ghscripts/publish_package.ts
@@ -3,16 +3,13 @@ import shell from 'shelljs';
 
 function publishPackage() {
     let build_dir = get_build_dir();
-    
+
     let cmd = `npm publish`;
 
     console.log(cmd);
-    
+
     shell.cd(build_dir);
-    let err = shell.exec(cmd).stderr;
-    if (err) {
-        throw new Error(err);
-    }
+    shell.exec(cmd, { fatal: true })
 }
 
 publishPackage();
